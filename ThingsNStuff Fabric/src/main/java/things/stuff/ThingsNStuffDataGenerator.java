ec4f9b1c5374f27d2f247c810e38ee5fa79a52f5
StarBladeModding <dracozean@gmail.com>
2025-11-24 20:08:45 -0500

    Upload files

---
+++ ThingsNStuff Fabric/src/main/java/things/stuff/ThingsNStuffDataGenerator.java
@@ -0,0 +1,11 @@
+package things.stuff;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ThingsNStuffDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
